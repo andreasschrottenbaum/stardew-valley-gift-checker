dd058657422370cb8f242eccbc991640401cfb1a
Andreas Schrottenbaum <andreas.schrottenbaum@3q.video>
2021-11-24 00:35:39 +0100

    changed logo url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,12 @@ import FileSelect from './FileSelect'
 function Header({ setPlayer }) {
   return (
     <header className='svgc-header'>
-      <img src='/logo.png' width='125' height='57' alt='Stardew Valley Logo' />
+      <img
+        src='https://stardewvalley.net/wp-content/uploads/2017/12/main_logo.png'
+        width='125'
+        height='57'
+        alt='Stardew Valley Logo'
+      />
 
       <h1>
         <span className='visually-hidden'>Stardew Valley</span> Gift Checker
